6992d69fb27b39351068336eb219f03a8957f303
Beninoonet <benjamin.breyne@proton.me>
2025-10-29 14:46:16 +0100

    Init a angular project

---
+++ src/app/pic-snap/pic-snap.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pic-snap',
+  imports: [],
+  templateUrl: './pic-snap.html',
+  styleUrl: './pic-snap.scss',
+})
+export class PicSnap {
+
+}
